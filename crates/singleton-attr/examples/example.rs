39bdd56a9c6cf481c7f806f0da45defebb4f9d17
Dimitriy <43708417+ay0ks@users.noreply.github.com>
2024-08-20 21:02:31 +0200

    feat: proc-macro wrapper

---
+++ crates/singleton-attr/examples/example.rs
@@ -0,0 +1,25 @@
+use singleton_attr::singleton;
+
+#[singleton]
+#[derive(Debug, Default)]
+pub struct Config {
+    pub a: i32,
+    pub b: i32,
+    pub c: String,
+}
+
+fn main() {
+    let config_1 = Config::get_instance();
+
+    println!("--BEFORE: {:#?}", config_1);
+    {
+        let mut config_1_lock = config_1.lock().unwrap();
+        config_1_lock.a = 123;
+        config_1_lock.b = -123;
+        config_1_lock.c = "Hello, World!".to_string();
+    }
+    println!("--AFTER: {:#?}", config_1);
+
+    let config_2 = Config::get_instance();
+    println!("--BEFORE: {:#?}", config_2)
+}
---
+++ crates/singleton-attr/src/lib.rs
@@ -0,0 +1,3 @@
+extern crate singleton_attr_proc_macro;
+pub use singleton_attr_proc_macro::singleton;
+pub use atomic_ref;
