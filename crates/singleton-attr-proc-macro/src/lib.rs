50626041e2260579b6c3d4a413bb280baf39219c
Dimitriy <43708417+ay0ks@users.noreply.github.com>
2024-08-21 10:52:46 +0000

    feat: 0.1.1

--- crates/singleton-attr-proc-macro/src/lib.rs
+++ crates/singleton-attr-proc-macro/src/lib.rs
@@ -1,11 +1,21 @@
+#![feature(proc_macro_span)]
 extern crate proc_macro;
-use proc_macro::TokenStream;
-use quote::{quote, format_ident};
-use syn::{parse_macro_input, spanned::Spanned, ItemStruct, Visibility, Token};
+use proc_macro::{Span, TokenStream};
+use quote::{format_ident, quote};
 use rand::{self, distributions::Alphanumeric, Rng};
+use syn::{parse_macro_input, spanned::Spanned, ItemStruct, Token, Visibility};
 
 #[proc_macro_attribute]
 pub fn singleton(_attr: TokenStream, item: TokenStream) -> TokenStream {
+    let span = Span::call_site();
+    let source_path = span.source_file().path();
+    let source_file = source_path.as_os_str().to_str().unwrap();
+
+    let source_file = source_file
+        .chars()
+        .map(|c| if c.is_ascii_alphanumeric() { c } else { '_' })
+        .collect::<String>();
+
     let mut input = parse_macro_input!(item as ItemStruct);
 
     let prefix: String = rand::thread_rng()
@@ -15,47 +25,58 @@ pub fn singleton(_attr: TokenStream, item: TokenStream) -> TokenStream {
         .collect();
 
     let name = &input.ident;
-    let name_prefix = format!("___S{}", prefix);
-    let (mod_name, init_name, instance_name) = (
-        format_ident!("{}_{}_", name_prefix, name),
-        format_ident!("{}", format!("{}_{}_INIT_", name_prefix, name).to_ascii_uppercase()),
-        format_ident!("{}", format!("{}_{}_INSTANCE_", name_prefix, name).to_ascii_uppercase()),
+    let name_prefix = format!("__{}_S{}", source_file, prefix);
+    let (mod_name, instance_name) = (
+        format_ident!(
+            "{}",
+            format!("{}_{}_", name_prefix, name).to_ascii_lowercase()
+        ),
+        format_ident!(
+            "{}",
+            format!("{}_{}_INSTANCE_", name_prefix, name).to_ascii_uppercase()
+        ),
     );
     let original_vis = input.vis.clone();
     input.vis = Visibility::Public(Token![pub](input.vis.span()));
 
     TokenStream::from(quote! {
         mod #mod_name {
+            use std::{alloc::{self, Layout}, ptr};
+            use singleton_attr::traits::Singleton;
             use super::*;
-            use std::sync::{Arc, Mutex, Once};
 
-            static #init_name: Once = Once::new();
-            static mut #instance_name: Option<Arc<Mutex<#name>>> = None;
-
+            static mut #instance_name: *mut #name = ptr::null_mut();
             #input
 
-            impl #name {
+            impl Singleton for #name {
                 #[inline]
-                pub unsafe fn init_instance(value: Self) {
-                    #init_name.call_once(|| unsafe { 
-                        #instance_name = Some(Arc::new(Mutex::new(value)));
-                    });
+                fn init_instance(instance: Self) {
+                    unsafe {
+                        #instance_name = alloc::alloc(Layout::new::<Self>()) as *mut Self;
+                        ptr::write_volatile(#instance_name, instance);
+                    }
                 }
 
                 #[inline]
-                pub fn get_instance() -> Arc<Mutex<Self>> {
-                    Self::init_instance(Self::default());
-                    unsafe { #instance_name.clone().unwrap() }
+                fn get_instance() -> &'static mut Self {
+                    unsafe {
+                        if #instance_name.is_null() {
+                            Self::init_instance(Self::default());
+                        }
+                        &mut *#instance_name
+                    }
                 }
 
                 #[inline]
-                fn clone(&self) -> Arc<Mutex<Self>> {
-                    Self::get_instance();
+                fn clone_instance(&self) -> &'static mut Self {
+                    Self::get_instance()
                 }
             }
 
             impl Drop for #name {
-                fn drop(&self) {}
+                fn drop(&mut self) {
+                    unsafe { alloc::dealloc(#instance_name as *mut u8, Layout::new::<Self>()); }
+                }
             }
         }
         #original_vis use #mod_name::#name;
