d9d50acf4737335291a3bb11f6c539e682c8e32a
Dimitriy <43708417+ay0ks@users.noreply.github.com>
2024-08-21 11:28:49 +0000

    feat: 0.1.1

--- crates/singleton-attr/src/lib.rs
+++ crates/singleton-attr/src/lib.rs
@@ -1,3 +1,5 @@
+#![allow(internal_features)]
+#![feature(negative_impls, negative_bounds)]
 pub mod traits;
 extern crate singleton_attr_proc_macro;
 pub use singleton_attr_proc_macro::singleton;
--- crates/singleton-attr/src/traits.rs
+++ crates/singleton-attr/src/traits.rs
@@ -1,4 +1,9 @@
-pub trait Singleton {
+/// Singleton
+///
+/// `Singleton::init_instance` - (re)initialize the singleton, prefer to use this if you want more control with the initial instance.
+/// `Singleton::get_instance` - get the singleton instance, if the instance is not initialized, it will initialize it with the default value.
+/// `Singleton::clone_instance` - get the singleton instance, use instead of `clone`, calls `Singleton::get_instance` under the hood.
+pub trait Singleton: Default {
     fn init_instance(instance: Self);
     fn get_instance() -> &'static mut Self;
     fn clone_instance(&self) -> &'static mut Self;
